065c826f6e57689b0f2458afbbe125525ae8501b
gaL <94966314+gaL236@users.noreply.github.com>
2025-05-22 15:00:24 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -9,16 +9,7 @@ app = Flask(__name__)
                                                                                                                                        
 @app.route('/')
 def hello_world():
-    return render_template('hello.html') #Comm2
-
-key = Fernet.generate_key()
-f = Fernet(key)
-
-@app.route('/encrypt/<string:valeur>')
-def encryptage(valeur):
-    valeur_bytes = valeur.encode()  # Conversion str -> bytes
-    token = f.encrypt(valeur_bytes)  # Encrypt la valeur
-    return f"Valeur encryptée : {token.decode()}"  # Retourne le token en str
+    return render_template('hello.html') #Comm3
                                                                                                                                                      
 if __name__ == "__main__":
   app.run(debug=True)
